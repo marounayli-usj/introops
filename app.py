7abcaff5a229624572f95d01ae29f195c326ac7b
marounayli-usj <maroun.ayli1@usj.edu.lb>
2023-10-05 18:09:29 +0300

    fixed repo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World from CICD and now we have a full pipeline"
+    return "Hello, World from CICD and now we have a full pipeline with no errors!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
